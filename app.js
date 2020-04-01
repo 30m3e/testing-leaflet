56e3bc60a990e630d0f46bed45b4410689fc294c
g1th <yungky.g1@gmail.com>
2020-04-02 01:06:26 +0630

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+<script
+  src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
+  integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
+  crossorigin=""
+></script>;
+
+var mymap = L.map("mapid").setView([51.505, -0.09], 13);
+
+L.tileLayer(
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: "mapbox/streets-v11",
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken:
+      "pk.eyJ1IjoiZzF0aCIsImEiOiJjazhnZjlrZ2MwMTE0M2VzNnlvOHF6NDMzIn0.m10Zqn_6vXjC5IkBr_-q2Q"
+  }
+).addTo(mymap);
