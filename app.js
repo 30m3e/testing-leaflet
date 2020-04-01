585195c2b1c4fed9ee6e8d14ed312f62ed263e4f
g1th <yungky.g1@gmail.com>
2020-04-02 01:44:03 +0630

    revised app.js

--- app.js
+++ app.js
@@ -2,16 +2,6 @@
 
 var mymap = L.map("mapid").setView([51.505, -0.09], 13);
 
-L.tileLayer(
-  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-  {
-    attribution:
-      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: "mapbox/streets-v11",
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken:
-      "pk.eyJ1IjoiZzF0aCIsImEiOiJjazhnZjlrZ2MwMTE0M2VzNnlvOHF6NDMzIn0.m10Zqn_6vXjC5IkBr_-q2Q"
-  }
-).addTo(mymap);
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mymap);
